bcdc2b9681ab6ef958a2992bded941917a9f7102
Alok kumar <75192701+alok007kr@users.noreply.github.com>
2025-04-01 23:33:53 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Header from "./components/Header/Header";
 import About from "./components/About/About";
 import Skills from "./components/Skills/Skills";
@@ -8,31 +8,35 @@ import Projects from "./components/Projects/Projects";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
 
-const Layout = () => (
-  <>
-    <Header />
-    <About />
-    <Skills />
-    <Experience />
-    <Projects />
-    <Contact />
-    <Footer />
-  </>
-);
+const ScrollToSection = () => {
+  const location = useLocation();
+
+  React.useEffect(() => {
+    if (location.hash) {
+      const element = document.getElementById(location.hash.substring(1));
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  }, [location]);
+
+  return null;
+};
 
 const App = () => {
   return (
     <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<About />} /> {/* Default page */}
-          <Route path="/about" element={<About />} />
-          <Route path="/skills" element={<Skills />} />
-          <Route path="/experience" element={<Experience />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </Layout>
+      <ScrollToSection /> {/* This ensures smooth scrolling */}
+      <Header />
+      <div id="about"><About /></div>
+      <div id="skills"><Skills /></div>
+      <div id="experience"><Experience /></div>
+      <div id="projects"><Projects /></div>
+      <div id="contact"><Contact /></div>
+      <Footer />
+      <Routes>
+        <Route path="/:section" element={null} />
+      </Routes>
     </Router>
   );
 };
